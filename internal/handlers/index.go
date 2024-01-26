cbde00c9f5c27083ad548d3ffe9696c015bc2687
Johan Siebens <johan.siebens@gmail.com>
2024-01-26 09:19:58 +0100

    chore: replace duplicate template code with templ

--- internal/handlers/index.go
+++ internal/handlers/index.go
@@ -1,17 +1,13 @@
 package handlers
 
 import (
+	tpl "github.com/jsiebens/ionscale/internal/templates"
 	"github.com/jsiebens/ionscale/internal/version"
 	"github.com/labstack/echo/v4"
 )
 
 func IndexHandler(code int) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		info, s := version.GetReleaseInfo()
-		data := map[string]interface{}{
-			"Version":  info,
-			"Revision": s,
-		}
-		return c.Render(code, "index.html", data)
+		return c.Render(code, "", tpl.Index(version.GetReleaseInfo()))
 	}
 }
